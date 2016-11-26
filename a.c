8b096257d4da04c62dbb7068050e608ecdf7df29
David <david.a.rodriguez@oracle.com>
2016-11-26 15:17:35 -0800

    All algorithms in a single file

--- a.c
+++ a.c
@@ -14,24 +14,24 @@ void frobenius1(int a, int b){
 // Unknown algorithm
 void frobenius2(int a, int b, int c, int d){
   bool f[10001],ff[1010001];
-  int i,j,aryLength,max,t,count,flag;
-  aryLength = 4;
-  int ary[aryLength];
+  int i,j,numsLength,max,t,count,flag;
+  numsLength = 4;
+  int nums[numsLength];
   memset(f,0,sizeof(f));
   memset(ff,0,sizeof(ff));
 
-  ary[0] = a; f[a] = 1;
-  ary[1] = b; f[b] = 1;
-  ary[2] = c; f[c] = 1;
-  ary[3] = d; f[d] = 1;
+  nums[0] = a; f[a] = 1;
+  nums[1] = b; f[b] = 1;
+  nums[2] = c; f[c] = 1;
+  nums[3] = d; f[d] = 1;
 
   ff[0]=1;
   count=1;
   max=0;
   for(i=0; i < 1000000;i++){
     flag=0;
-    for(j = 0; j < aryLength; j++){
-      if( i-ary[j] >= 0 && ff[i-ary[j]] == 1) {
+    for(j = 0; j < numsLength; j++){
+      if( i-nums[j] >= 0 && ff[i-nums[j]] == 1) {
         ff[i]=1;
         count++;
         flag=1;
@@ -45,8 +45,8 @@ void frobenius2(int a, int b, int c, int d){
   t=0;
   for(i=1000000; i < 1010000; i++){
     flag=0;
-    for(j=1; j <= aryLength; j++){
-      if(i-ary[j]>=0 && ff[i-ary[j]]) {
+    for(j=1; j <= numsLength; j++){
+      if(i-nums[j]>=0 && ff[i-nums[j]]) {
         flag=1;
         ff[i]=1;
         break;
@@ -65,12 +65,18 @@ void frobenius2(int a, int b, int c, int d){
 }
 
 // Sebastian Böcker & Zsuzsanna Lipták
-void frobenius3(int* nums){
-  int i,j,d,r,n,q,p,max;
+void frobenius3(int w, int x, int y, int z){
+  int i,j,d,r,n,q,p,max,numsLength;
+  int nums[numsLength];
+  numsLength = 4;
+  nums[0] = w;
+  nums[1] = x;
+  nums[2] = y;
+  nums[3] = z;
   int ns[nums[0]];
   memset(ns,-1,sizeof(ns));
   ns[0] = 0;
-  for (int i = 1; i < sizeof(nums); i++) {
+  for (int i = 1; i < numsLength; i++) {
       d = gcd(nums[0], nums[i]);
       for (r = 0; r < d; r++) {
           n = -1;
@@ -113,7 +119,5 @@ void frobenius3(int* nums){
 int main(){
   frobenius1(4, 7);
   frobenius2(4, 6, 9, 20);
-  int a[4] = {4, 6, 9, 20};
-  frobenius3(a);
-
+  frobenius3(4, 6, 9, 20);
 }
